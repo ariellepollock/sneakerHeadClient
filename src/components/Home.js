b37a4d6288f68bf181625215fa2844831c431005
ariellepollock <ariellecpollock@gmail.com>
2024-02-12 12:35:26 -0600

    add loading screen and api call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { getAllSneakers } from "../api/sneaker"
+import LoadingScreen from './shared/LoadingScreen'
 
 const Home = (props) => {
 	const { msgAlert, user } = props
@@ -9,13 +10,19 @@ const Home = (props) => {
 
 	useEffect(() => {
 		getAllSneakers()
-			.then(res => console.log('sneakers: \n', res.data.sneakers))
+			// .then(res => console.log('sneakers from axios call: \n', res.data.sneakers))
+			.then(res => {
+				console.log('use Effect hook ran')
+				setSneakers(res.data.sneakers)
+			})
 			.catch(error => console.error)
 	}, [])
-
+	console.log('the sneakers in Home: \n', sneakers)
 	return (
 		<>
 			<h2>Home Page</h2>
+			{ user !== null ? <h5>Hello {user.email}</h5> : null }
+			{sneakers === null ? <LoadingScreen /> : <p>{sneakers[0].name}</p>}
 		</>
 	)
 }
---
+++ src/components/shared/LoadingScreen.jsx
@@ -0,0 +1,9 @@
+import { Spinner } from 'react-bootstrap'
+
+const LoadingScreen = () => (
+    <div className="container-sm" style={{ textAlign: 'center' }}>
+        <Spinner role='status' animation='border' variant='info'/>
+    </div>
+)
+
+export default LoadingScreen
